fd117de4120c72ed7f6c1b0e22eeeea269633088
David Tomberlin <siyegen@gmail.com>
2012-12-10 23:45:29 -0800

    loading simple js through express

---
+++ public/js/look.js
@@ -0,0 +1,3 @@
+window.look = function() {
+  alert('moo look');
+};
--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.bodyParser());
+app.use(express.errorHandler());
+//app.use(express.favicon(__dirname + '/public/images/favicon.ico', { maxAge: 2592000000 }));
+app.use(express.logger());
+//app.use(express.cookieParser('it is a secret to everybody'));
+////app.use(express.session({secret: 'really what',  cookie: { maxAge: 60000 }}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/x'));
+
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  res.send('moo');
+  res.render('index');
 });
 
 app.listen(PORT, function(req, res) {
